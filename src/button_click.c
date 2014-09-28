76a9789ab2e6f732e37fdf9eeda263243a1958d5
iweiny <iweiny@gmail.com>
2014-09-28 08:50:28 -0700

    Change App to be a WeightWatch
    Use the buttons to increment weight by 1/10 increments and display the weight.
    
    Increase font to be something much bigger than the system font.
    Persistence is not working yet.

--- src/button_click.c
+++ src/button_click.c
@@ -3,16 +3,52 @@
 static Window *window;
 static TextLayer *text_layer;
 
+/**
+ * Weight handler code.
+ */
+uint16_t w1 = 150;
+int8_t w2 = 0;
+char wstring[] = "xxxx.y";
+
+static void display_weight(void) {
+	snprintf(wstring, sizeof(wstring), "%4d.%1d", w1, w2);
+	text_layer_set_text(text_layer, wstring);
+}
+
+static void increment_weight(void) {
+	w2++;
+	if (w2 >= 10) {
+		w1++;
+		w2 = 0;
+	}
+}
+
+static void decriment_weight(void) {
+	w2--;
+	if (w2 < 0) {
+		w1--;
+		w2 = 9;
+	}
+}
+	
+
+/**
+ * Button Handler code
+ */
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+	display_weight();
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+  //text_layer_set_text(text_layer, "Up");
+	increment_weight();
+	display_weight();
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+  //text_layer_set_text(text_layer, "Down");
+	decriment_weight();
+	display_weight();
 }
 
 static void click_config_provider(void *context) {
@@ -25,8 +61,10 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
+  text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w, bounds.size.h } });
+	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD));
+  //text_layer_set_text(text_layer, "Press a button");
+	display_weight();
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
