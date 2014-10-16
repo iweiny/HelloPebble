fef73c2891e1840702ea4e7734572c3c432c2f64
iweiny <iweiny@gmail.com>
2014-10-15 19:29:50 -0700

    Make up/down buttons repeating
    The buttons will be called repeatedly when pushed and held.
    
    This works well for adjusting the weight large values.

--- src/button_click.c
+++ src/button_click.c
@@ -83,8 +83,8 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
 
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_UP, 30, up_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_DOWN, 30, down_click_handler);
 }
 
 static void window_load(Window *window) {
